7ad9d5df3ce6764edec7c09cb655208dd3839b1f
SUJVERSE <sujsh330@gmail.com>
2026-08-24 23:13:56 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const continueButton = document.getElementById("continueButton");
 
+continueButton.addEventListener("click", () => {
+
+    continueButton.innerText = "Coming soon... 🌙✨";
+
+    continueButton.style.transform = "scale(0.95)";
+
+    setTimeout(() => {
+        continueButton.innerText = "There's more... ✨";
+        continueButton.style.transform = "scale(1)";
+    }, 1000);
+
+});
